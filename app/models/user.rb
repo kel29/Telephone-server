a755630f1039556347751a172ded4901b36f6125
Krista Loven <krista.loven.87@gmail.com>
2019-12-03 11:57:22 -0800

    removed seed, updated for fb login compatibility

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
   has_many :games
   has_many :game_rounds, through: :games
-  validates :name, presence: true, uniqueness: true
-  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :name, presence: true
 end
--- db/seeds.rb
+++
@@ -1,6 +0,0 @@
-User.create(name: 'krista', email: 'k@k.com')
-User.create(name: 'bog', email: 'b@b.com')
-Game.create(user_id: 1)
-Game.create(user_id: 2)
-GameRound.create(game_id: 1, contributor: 'kel', sentence: 'A human is doing yoga')
-GameRound.create(game_id: 2, contributor: 'vygo', sentence: 'The cow jumped over the spoon')
