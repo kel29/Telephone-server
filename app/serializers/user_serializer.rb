34717d26314151bffa3e485a56319b115706a9ca
Krista Loven <krista.loven.87@gmail.com>
2020-01-08 16:28:12 -0800

    Create User serializer with games and game_rounds

---
+++ app/serializers/user_serializer.rb
@@ -0,0 +1,24 @@
+class UserSerializer < ActiveModel::Serializer
+  attributes :id, :name, :facebook_id, :games
+
+  def games
+    object.games.map do |game|
+      {
+        game_id: game.id,
+        created_at: game.created_at,
+        game_rounds: game_rounds(game)
+      }
+    end
+  end
+
+  def game_rounds(game)
+    game.game_rounds.map do |round|
+      {
+        id: round.id,
+        sentence: round.sentence,
+        drawing: round.drawing,
+        created_at: round.created_at
+      }
+    end
+  end
+end
