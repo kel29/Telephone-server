5bd8d7da4bd5ecfbe857b8ecc7da4e5399534679
Krista Loven <krista.loven.87@gmail.com>
2020-02-19 17:54:17 -0800

    Update routes to include update

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :game_rounds, only: [:index, :create]
-  resources :games, only: [:index, :create, :show]
+  resources :games, only: [:index, :create, :update, :show]
   resources :users, only: [:index, :create, :update, :show]
 end
